d9be45357f2a004604ff4ef848779f5adb583bbf
hardboiled65 <hardboiled65@gmail.com>
2018-03-16 15:56:16 +0900

    Remove unused back-end template.

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
-from property_names import property_names
 
 class App(Flask):
     jinja_options = Flask.jinja_options.copy()
@@ -16,18 +15,18 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/<path:demo>', strict_slashes=False)
 def index(demo, cp=None):
     print('demo is: ' + str(demo))
-    return render_template('index.html', property_names=property_names)
+    return render_template('index.html')
 
 @app.route('/browse')
 @app.route('/browse/<cp>')
 def browse_index(cp=None):
-    return render_template('index.html', property_names=property_names)
+    return render_template('index.html')
 
 @app.route('/segmentation', defaults={'_type': ''})
 @app.route('/segmentation/<_type>')
 @app.route('/segmentation/<_type>/<text>')
 def segmentation_index(_type, text=''):
-    return render_template('index.html', property_names=property_names)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port=5000)
